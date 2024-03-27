f67fd3e73ec1ae5f20f3288fde575e32fdb15013
Brock Vojkovic <github@vojk.au>
2024-03-27 08:05:02 +0000

    Render page

--- searx/webapp.py
+++ searx/webapp.py
@@ -615,7 +615,11 @@ def health():
 
 @app.route('/privacy', methods=['GET'])
 def privacy_policy():
-    return Response('OK', mimetype='text/plain')
+    return send_from_directory(
+        os.path.join(app.root_path, settings['ui']['static_path'], 'pages', 'privacy'),  # pyright: ignore
+        'index.html',
+        mimetype='text/html',
+    )
 
 
 @app.route('/client<token>.css', methods=['GET', 'POST'])
